3a182eae42d1cb7bafcdf610425de8696292ba51
cksdnde <113408953+cksdnde@users.noreply.github.com>
2024-07-29 14:22:45 +0900

    [level 0] Title: 양꼬치, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.py
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.py
@@ -1,7 +1,2 @@
-def solution(num1, num2):
-    cost = num1 * 12000
-    fdrinks = num1 // 10
-    pdrinks = max(0, num2 - fdrinks) 
-    dcost = pdrinks * 2000
-    result = cost + dcost
-    return result
+def solution(n, k):
+    return 12000 * n + 2000 * (k - n // 10)
