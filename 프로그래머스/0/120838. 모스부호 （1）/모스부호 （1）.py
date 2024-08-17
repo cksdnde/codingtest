05e0a57efe4d34c85ee932e6f482db7e93a9a3ca
cksdnde <113408953+cksdnde@users.noreply.github.com>
2024-08-17 21:40:07 +0900

    [level 0] Title: 모스부호 (1), Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.py
@@ -0,0 +1,12 @@
+morse = {
+    '.-': 'a', '-...': 'b', '-.-.': 'c', '-..': 'd', '.': 'e', '..-.': 'f',
+    '--.': 'g', '....': 'h', '..': 'i', '.---': 'j', '-.-': 'k', '.-..': 'l',
+    '--': 'm', '-.': 'n', '---': 'o', '.--.': 'p', '--.-': 'q', '.-.': 'r',
+    '...': 's', '-': 't', '..-': 'u', '...-': 'v', '.--': 'w', '-..-': 'x',
+    '-.--': 'y', '--..': 'z'
+}
+
+def solution(letter):
+    words = letter.split(" ")  
+    decoded_message = ''.join(morse[word] for word in words)
+    return decoded_message
